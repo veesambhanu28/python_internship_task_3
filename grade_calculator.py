d9908266ade1635d44593aefa2ccf834a48b4204
Veesam Bhanu <veesambhanu28@gmail.com>
2026-01-21 09:58:23 +0530

    Add files via upload
    Added grade calculator program

---
+++ grade_calculator.py
@@ -0,0 +1,19 @@
+# grade_calculator.py
+
+marks = int(input("Enter marks (0-100): "))
+
+if marks < 0 or marks > 100:
+    print("Invalid marks")
+
+else:
+    
+    if marks >= 90:
+        print("Grade A+")
+    elif marks >= 75:
+        print("Grade A")
+    elif marks >= 60:
+        print("Grade B")
+    elif marks >= 40:
+        print("Grade C")
+    else:
+        print("Fail")
